73c799cc4e6524c5ff4cec8b4d542c3a355b6055
fcollazoabadia <f.collazoabadia@gmail.com>
2023-07-05 16:33:47 -0500

    finished generator

--- index.js
+++ index.js
@@ -1,13 +1,103 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const generateREADME = ({ title, description, installation, usage, credits, license, tests, github, email}) =>
+  `# ${title}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+  ## ${description}
 
-// TODO: Create a function to initialize app
-function init() {}
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Github](#github)
+  - [Email](#email)
+  - [Tests](#tests)
 
-// Function call to initialize app
-init();
+  ## Installation
+  
+  ${installation}
+  
+  ## Usage
+  
+  ${usage}
+  
+  ## Credits
+  
+  ${credits}
+
+  ## License
+  
+  ${license}
+  
+  ---
+
+  ##Questions
+
+  My GitHub profile: ${github}
+
+  Reach me at ${email} for any additional questions.
+
+  ---
+  
+  ## Tests
+
+  ${tests}`;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Project Title:',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a short description explaining the what, why, and how of your project.',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the steps required to install your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Provide instructions and examples for use. Include screenshots as needed.',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'Project Licenses?',
+    },
+    {
+      type: 'input',
+      name: 'credits',
+      message: 'Other credits?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are some tests for your project?',
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'GitHub Link:',
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Email address:',
+    },
+  ])
+  .then((answers) => {
+    const READMEPageContent = generateREADME(answers);
+
+    fs.writeFile('README.md', READMEPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully generated README.md!')
+    );
+  });
